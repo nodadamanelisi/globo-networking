dae148744804f85d97bb318ab7b69ef818fb7853
nodadamane <nodadamanelisi@gmail.com>
2026-06-07 22:43:12 +0200

    add subnet and tags

--- variables.tf
+++ variables.tf
@@ -20,6 +20,11 @@ variable "environment" {
   default     = "development"
 }
 
+variable "billing_code" {
+  type        = string
+  description = "(Optional) Billing code to use for all resources in this module. Default: globo-dev"
+}
+
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
